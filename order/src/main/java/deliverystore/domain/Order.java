cf07e00e35baa64edf5cf8825addc59d6b02dc37
ijsong-do <ijsong@sicc.co.kr>
2022-12-10 11:37:17 +0000

    c

--- order/src/main/java/deliverystore/domain/Order.java
+++ order/src/main/java/deliverystore/domain/Order.java
@@ -76,7 +76,7 @@ public class Order  {
    
         // mappings goes here
         OrderApplication.applicationContext.getBean(deliverystore.external.PaymentService.class)
-            .pay(Long.valueOf(getId()), payCommand); 
+            .pay(getId(), payCommand); 
 
         OrderPlaced orderPlaced = new OrderPlaced(this);
         orderPlaced.publishAfterCommit();
