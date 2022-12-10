d290007f7ddc03f5438d7530e71ff864fb61f6a4
ijsong-do <ijsong@sicc.co.kr>
2022-12-10 12:33:00 +0000

    c

--- order/src/main/java/deliverystore/external/PaymentService.java
+++ order/src/main/java/deliverystore/external/PaymentService.java
@@ -8,9 +8,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.Date;
 
-
 @FeignClient(name = "payment", url = "${api.url.payment}")
 public interface PaymentService {
-    @RequestMapping(method= RequestMethod.PUT, path="/payments/{id}/pay")
-    public void pay(@PathVariable("id") Long id, @RequestBody PayCommand payCommand );
+    @RequestMapping(method = RequestMethod.POST, path = "/payments")
+    public void pay(@RequestBody Payment payment);
 }
